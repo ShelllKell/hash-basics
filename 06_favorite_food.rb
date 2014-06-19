4298081b57010e833999af13abc2b151dbd925b4
Shelby Kelly <shelby.kelly@colorado.edu>
2014-06-19 14:24:26 -0600

    Adding favorite foods

--- 06_favorite_food.rb
+++ 06_favorite_food.rb
@@ -1,3 +1,6 @@
 require_relative "person"
 
 # Print Bjorn's favorite foods. It should read "Bjorn's favorite foods are sushi, hamburgers, and mexican food."
+
+print "#{BJORN_BORG["first_name"]}'s favorite foods are #{BJORN_BORG["favorite_foods"][0]}, #{BJORN_BORG["favorite_foods"][1]}, and #{BJORN_BORG["favorite_foods"][2]}."
+
